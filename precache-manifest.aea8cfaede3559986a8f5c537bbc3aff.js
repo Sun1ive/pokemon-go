69cc6ba5995f100ae52d609dd1deb0356dd8f0d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-09 05:12:29 +0000

    Deploy Sun1ive/pokemon-go to github.com/Sun1ive/pokemon-go.git:gh-pages

---
+++ precache-manifest.aea8cfaede3559986a8f5c537bbc3aff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "884c83f8ebb5bf0ebd68",
+    "url": "/pokemon-go/js/chunk-vendors.884c83f8.js"
+  },
+  {
+    "revision": "7846b5853bc66ddacd53",
+    "url": "/pokemon-go/js/app.7846b585.js"
+  },
+  {
+    "revision": "4dc9e9242aa087f76ae6c48877006a47",
+    "url": "/pokemon-go/index.html"
+  },
+  {
+    "revision": "884c83f8ebb5bf0ebd68",
+    "url": "/pokemon-go/css/chunk-vendors.4e53556a.css"
+  },
+  {
+    "revision": "7846b5853bc66ddacd53",
+    "url": "/pokemon-go/css/app.49165439.css"
+  }
+];
